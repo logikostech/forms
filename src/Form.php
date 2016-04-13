a35a8fd17504ead80de2ca6520768e6dff3e5790
tempcke <t.bitbucket@logikostech.net>
2016-04-13 12:03:10 -0400

    cleanup

--- src/Form.php
+++ src/Form.php
@@ -8,16 +8,16 @@ use Phalcon\Forms\Element\Hidden;
 
 class Form extends phForm {
   public function action($action) {
-  	parent::setAction($action);
+    parent::setAction($action);
     return $this;
   }
   public function method($method='POST') {
-  	$hidden = new Hidden('_method');
-  	$hidden->setAttribue('value',strtoupper($method));
-  	$this->add($hidden);
-  	return $this;
+    $hidden = new Hidden('_method');
+    $hidden->setAttribue('value',strtoupper($method));
+    $this->add($hidden);
+    return $this;
   }
   public function fieldlist() {
-  	var_dump($this->getElements());
+    var_dump($this->getElements());
   }
 }
