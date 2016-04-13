d322a75d1bb05eb4867b6804ccfcf3376b9bb194
tempcke <t.bitbucket@logikostech.net>
2016-04-13 11:53:23 -0400

    initial commit

---
+++ src/Form.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Logikos\Forms;
+
+use Phalcon\Forms\Form as phForm;
+use Phalcon\Forms\ElementInterface;
+use Phalcon\Forms\Element\Hidden;
+
+class Form extends phForm {
+  public function action($action) {
+  	parent::setAction($action);
+    return $this;
+  }
+  public function method($method='POST') {
+  	$hidden = new Hidden('_method');
+  	$hidden->setAttribue('value',strtoupper($method));
+  	$this->add($hidden);
+  	return $this;
+  }
+  public function fieldlist() {
+  	var_dump($this->getElements());
+  }
+}
