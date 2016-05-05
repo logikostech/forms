ee518ea1165a01bebdbb64ff12f7524a09529ddf
tempcke <t.bitbucket@logikostech.net>
2016-05-05 16:32:15 -0400

    added setDecorationTemplate(), renderDecorated(), setView()

--- src/Form.php
+++ src/Form.php
@@ -5,6 +5,8 @@ namespace Logikos\Forms;
 use Phalcon\Forms\Form as phForm;
 use Phalcon\Forms\ElementInterface;
 use Phalcon\Forms\Element\Hidden;
+use Phalcon\Mvc\ViewInterface;
+use Phalcon\Mvc\ViewBaseInterface;
 
 class Form extends phForm {
   
@@ -12,6 +14,7 @@ class Form extends phForm {
   protected $_method_element_name = '_method';
   protected $_valid_methods = ['POST','GET'];
   protected $_method = 'POST';
+  protected $_decoration_template;
   
   public function isValidMethod($method) {
     return in_array($method,$this->_valid_methods);
@@ -46,5 +49,40 @@ class Form extends phForm {
   }
   public function fieldlist() {
     return $this->getElements();
+  }
+  
+  public function setDecorationTemplate($path) {
+    $this->_decoration_template = $path;
+  }
+  
+  public function setView(ViewBaseInterface $view) {
+    $this->view = $view;
+  }
+  public function getView() {
+    if (isset($this->view) && $this->view instanceof ViewBaseInterface)
+      return $this->view;
+    return $this->getDI()->get('view');
+  }
+  public function renderDecorated($name, $attributes=null, $template=null) {
+    $template = $template ?: $this->_decoration_template;
+    
+    if (!$template)
+      return $this->render($name,$attributes);
+    
+    $element  = $this->get($name);
+    $messages = $this->getMessagesFor($element->getName()) ?: [];
+    
+    ob_start();
+    $this->getView()->partial(
+        $template,
+        [
+            'form'     => $this,
+            'element'  => $element,
+            'messages' => $messages
+        ]
+    );
+    $output = ob_get_contents();
+    ob_end_clean();
+    return $output;
   }
 }
